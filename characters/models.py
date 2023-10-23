04acd1be76efa56891f6e30ccfb8c1b35320b6a5
kos_zivenko <kos.zivenko@gmail.com>
2023-10-23 15:52:33 +0200

    added: model Character and migrations

---
+++ characters/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Character(models.Model):
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive"
+        DEAD = "Dead"
+        UNKNOWN = "unknown"
+
+    class GenderChoices(models.TextChoices):
+        FEMALE = "Female"
+        MALE = "Male"
+        GENDERLESS = "Genderless"
+        UNKNOWN = "unknown"
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=16, choices=StatusChoices.choices)
+    species = models.CharField(max_length=255)
+    gender = models.CharField(max_length=16, choices=GenderChoices.choices)
+    image = models.URLField(unique=True, max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
